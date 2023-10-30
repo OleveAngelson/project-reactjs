7fa5f6e2d032f33d9d5935e9b122c99d79ca7300
Mas Bro <89410848+OleveAngelson@users.noreply.github.com>
2023-10-31 00:37:29 +0800

    Update Todo.js

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fontawesome/react-fontawesome'
+import { FortAwesomeIcon } from '@fortawesome/react-fortawesome'
 import { faPenSquare, faTrashAlt } from '@fontawesome/free-solid-svg-icons'
 
 export const Todo = ({ task, deleteTodo, editTodo, toggleComplete }) => {
